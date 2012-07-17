33e4ae5ae3a2a2a275bf42e683fe23f4ba538d5e
Roberto Vasquez Angel <robotex@robotex.de>
2012-07-17 03:16:15 +0200

    Added rails 3.2.6 skeleton app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeMatrix::Application.config.secret_token = 'b3fcfb8727ec896179e433f351c0b367b7ca9607ce7895c7cf28d98bdcb4f5290681f09aa2d8ef95cc8262ef8961c64bd31c69bc5c1dad57c1258636a43fb4be'
